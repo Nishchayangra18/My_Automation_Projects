5af02d1debe106b8c63fdcce5863e0d8a77ad5bb
Nishchayangra18 <nishchayangra18@gmail.com>
2024-03-03 18:23:46 +0530

    Login to OrangeHRM test case

---
+++ Pages/Login_Page.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class LoginPage:
+    textbox_username_xpath = "//input[@name='username']"
+    textbox_password_xpath = "//input[@name='password']"
+    button_login_xpath = "//button[@type='submit']"
+    user_area_dropdown_xpath = "//span[@class='oxd-userdropdown-tab']"
+    button_logout_linktext = "Logout"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enterUsername(self, username):
+        wait = WebDriverWait(self.driver, 20)  # set a max wait time
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, self.textbox_username_xpath)))
+        element.send_keys(username)
+
+    def enterPassword(self, password):
+        wait = WebDriverWait(self.driver, 20)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, self.textbox_password_xpath)))
+        element.send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element(By.XPATH, self.button_login_xpath).click()
+
+    def clickLogout(self):
+        wait = WebDriverWait(self.driver, 20)
+        element = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, self.button_logout_linktext)))
+        element.click()
+
+    def clickuserdropdown(self):
+        self.driver.find_element(By.XPATH, self.user_area_dropdown_xpath).click()
