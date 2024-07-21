8b66842d2a32d05b9da67689a817b9a506d3838d
Nishchayangra18 <nishchayangra18@gmail.com>
2024-07-21 14:07:24 +0530

    Implemented Initial Rest Assured Test cases - Get, Post, Update, Delete

---
+++ RestAssuredTesting/src/test/java/Day_1/HTTP_Requests.java
@@ -0,0 +1,80 @@
+package Day_1;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+public class HTTP_Requests {
+	
+	int id;
+	
+	@Test(priority=1)
+	void getUsers()
+	{
+		given()
+		
+		.when()
+			.get("https://reqres.in/api/users?page=2")
+		
+		.then()
+			.statusCode(200)
+			.body("page",equalTo(2))
+			.log().all();
+	}
+	
+	@Test(priority=2)
+	void createUser()
+	{
+		HashMap hm = new HashMap();
+		hm.put("name", "Nishchay");
+		hm.put("job", "QA Engineer");
+		
+		id=given()
+			.contentType("application/json")
+			.body(hm)
+		
+		.when()
+			.post("https://reqres.in/api/users")
+			.jsonPath().getInt("id");
+		
+//		.then()
+//			.statusCode(201)
+//			.log().all();
+	}
+	
+	@Test(priority=3, dependsOnMethods= {"createUser"})
+	void updateUser()
+	{
+		HashMap hm = new HashMap();
+		hm.put("name", "Nishchay Angra");
+		hm.put("job", "Associate Software Engineer - 1");
+		
+		given()
+			.contentType("application/json")
+			.body(hm)
+		
+		.when()
+			.put("https://reqres.in/api/users/"+id)
+		
+		.then()
+			.statusCode(200)
+			.log().all();
+	}
+	
+	@Test(priority=4)
+	void deleteUser()
+	{
+		given()
+		
+		.when()
+			.delete("https://reqres.in/api/users/"+id)
+		
+		.then()
+			.statusCode(204)
+			.log().all();
+	}
+
+}
